fd610ce142df317154f90f8f923a851e008abe5c
alexander-russell <alexander.russell4@pm.me>
2025-07-12 23:30:04 +1000

    Minor tidying to viewer formatting

--- manager/templatetags/recipe_extras.py
+++ manager/templatetags/recipe_extras.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django import template
 from django.utils.html import conditional_escape
 from django.utils.safestring import mark_safe
@@ -6,10 +7,11 @@ import markdown
 
 register = template.Library()
 
+
 @register.filter(needs_autoescape=True)
 def wrap_scalable(value, autoescape=True):
     # This regex matches @<int> patterns
-    pattern = r'@(\d+(.\d+)?)'
+    pattern = r"@(\d+(.\d+)?)"
 
     if autoescape:
         esc = conditional_escape
@@ -23,13 +25,16 @@ def wrap_scalable(value, autoescape=True):
     # Use re.sub to replace the pattern with the wrapped version
     return mark_safe(re.sub(pattern, replace, esc(value)))
 
+
 @register.filter()
 def pluralise_unit(value, quantity):
     return value.singular if quantity <= 1 else value.plural
 
+
 @register.filter()
 def render_markdown(value):
     return mark_safe(markdown.markdown(value))
+
 
 @register.filter()
 def format_timer(value):
@@ -37,4 +42,29 @@ def format_timer(value):
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) / 60
     seconds = total_seconds % 60
-    return "%d:%02d:%02d" % (hours, minutes, seconds) if hours > 0 else "%02d:%02d" % (minutes, seconds)
+    return (
+        "%d:%02d:%02d" % (hours, minutes, seconds)
+        if hours > 0
+        else "%02d:%02d" % (minutes, seconds)
+    )
+
+
+@register.filter
+def detrail(value):
+    """Removes trailing zeros from a number (float or decimal)."""
+    return re.sub(r"0+$", "", value) if "." in str(value) else str(value)
+
+
+@register.filter
+def format_duration(value):
+    total_seconds = value.total_seconds()
+    hours = int(total_seconds // 3600)
+    minutes = int((total_seconds % 3600) / 60)
+
+    return f"{hours} hr " * (hours > 0) + f"{minutes} min" * (minutes > 0)
+    # if hours > 0 and minutes != 0:
+    #     return f"{hours} hr {minutes} min"
+    # elif hours > 0:
+    #     return f"{hours} hr"
+    # else:
+    #     return f"{minutes} min"
