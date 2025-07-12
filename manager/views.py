839847d4175740dafe9b0826cc0dee3103b9c9ae
alexander-russell <alexander.russell4@pm.me>
2025-07-13 01:15:47 +1000

    Add recipe view warnings

--- manager/views.py
+++ manager/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404, HttpResponse
+from datetime import date, datetime, timedelta
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
@@ -18,13 +19,20 @@ def contents(request):
 
 
 def view(request, recipe_slug):
+    # Get recipe
     recipe = get_object_or_404(Recipe, slug=recipe_slug)
+
+    # Calculate costs
     item_costs = [item.get_cost() for item in recipe.items.all()]
     total_cost = sum(
         item_cost["amount"] if item_cost["amount"] is not None else 0
         for item_cost in item_costs
     )
     cost_per_serve = total_cost / recipe.yield_quantity
+
+    # Determine if recipe hasn't been updated in over a month
+    stale = recipe.date_updated < date.today() - timedelta(days=60)
+
     return render(
         request,
         "manager/view/index.html",
@@ -33,5 +41,6 @@ def view(request, recipe_slug):
             "item_costs": item_costs,
             "total_cost": total_cost,
             "cost_per_serve": cost_per_serve,
+            "stale": stale,
         },
     )
