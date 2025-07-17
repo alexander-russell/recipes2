29674bf0dd2137f52f4ab89eaae2c8c0d70a0244
alexander-russell <alexander.russell4@pm.me>
2025-07-17 22:17:36 +1000

    Close overlay on escape keydown

--- manager/static/manager/viewer/overlays.js
+++ manager/static/manager/viewer/overlays.js
@@ -3,3 +3,10 @@ function toggleOverlay(event) {
     const overlay = document.querySelector(`#${overlayName}`);
     overlay.classList.toggle("hidden")
 }
+
+// Close any open overlay on press of escape key
+document.addEventListener("keydown", (event) => {
+    if (event.key == "Escape") {
+        document.querySelectorAll(".overlay-background:not(.hidden)").forEach(overlay => {overlay.classList.add("hidden")});
+    }
+});
