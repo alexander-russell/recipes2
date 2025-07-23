ee7a395f1c2389f0401eba2e382a7195ac1c28a3
alexander-russell <alexander.russell4@pm.me>
2025-07-24 01:51:06 +1000

    Format code

--- manager/static/manager/home/home.js
+++ manager/static/manager/home/home.js
@@ -1,16 +1,16 @@
-  const input = document.querySelector('input#name');
-  const items = document.querySelectorAll('.search-results li');
+const input = document.querySelector('input#name');
+const items = document.querySelectorAll('.search-results li');
 
-  input.addEventListener('input', () => {
+input.addEventListener('input', () => {
     const query = input.value.toLowerCase();
     let resultsCount = 0;
 
     items.forEach(li => {
-      if (li.querySelector('a').textContent.toLowerCase().includes(query) && resultsCount < 5) {
-          li.classList.remove("hidden");
-          resultsCount++;
-      } else {
-          li.classList.add("hidden");
-      }
+        if (li.querySelector('a').textContent.toLowerCase().includes(query) && resultsCount < 5) {
+            li.classList.remove("hidden");
+            resultsCount++;
+        } else {
+            li.classList.add("hidden");
+        }
     });
-  });
+});
