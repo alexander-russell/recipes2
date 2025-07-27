b1bc2979a5321b51f2f388e02d1e6c45871826f6
alexander-russell <alexander.russell4@pm.me>
2025-07-27 11:23:25 +1000

    Use .env for sensitive information, resolves #23

--- recipes/settings.py
+++ recipes/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 from pathlib import Path
+
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +30,7 @@ SECRET_KEY = 'django-insecure-%o@ikr^--=n!%0hb@q+ya+!kw27vi7-3dtbdq$)rz0qm9*imsy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
