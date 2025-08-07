9915b2668d292191b2d1fc397ff5c5be7c6a6a4b
alexander-russell <alexander.russell4@pm.me>
2025-08-08 09:52:42 +1000

    Tell django recipes2 was called manager, for migrations

--- recipes2/apps.py
+++ recipes2/apps.py
@@ -5,7 +5,8 @@ import logging
 class Recipes2Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "recipes2"
-
+    label = "manager" 
+    
     def ready(self):
         logging.warning("Importing recipes2.signals")
         import recipes2.signals
