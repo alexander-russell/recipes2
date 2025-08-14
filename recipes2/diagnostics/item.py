c67542bbeb082fafdf7ea7e6f1029a715e65d54b
alexander-russell <alexander.russell4@pm.me>
2025-08-15 08:14:43 +1000

    Add diagnostic test for items with zero quantity

--- recipes2/diagnostics/item.py
+++ recipes2/diagnostics/item.py
@@ -1,6 +1,7 @@
 from recipes2.models import Item, Recipe
 from django.db.models import F
 
+
 def run():
     return {
         "No Cost": {
@@ -18,7 +19,15 @@ def run():
             "data": (
                 Item.objects.filter(recipe__status=Recipe.Status.ACTIVE)
                 .exclude(group__isnull=True)
-                .exclude(recipe__id=F('group__recipe__id'))
+                .exclude(recipe__id=F("group__recipe__id"))
+            ),
+        },
+        "Zero Quantity But Unit Not Manual": {
+            "template": "recipes2/diagnostics/partials/_results_table_item_unit.html",
+            "data": (
+                Item.objects.filter(recipe__status=Recipe.Status.ACTIVE)
+                .filter(quantity=0)
+                .exclude(unit__name="manual")
             ),
         },
     }
